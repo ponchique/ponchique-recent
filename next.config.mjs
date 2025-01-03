91d7ceadffca62920418ea8042b1741fc35385f4
ponchique <anastasiia.norenko@gmail.com>
2025-01-03 11:22:19 +0100

    upd next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   output: "export",
   images: { unoptimized: true },
-  experimental: {
-    mdxRs: true,
-  },
   basePath: "",
 };
 
